a6bb159680f39760207cec15da56c2de7e47fb83
angelmora2004 <alexandermb@live.com>
2022-12-21 22:22:03 -0500

    Agregar archivos de proyecto.

---
+++ frmBuscaMinas.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace JuegoBuscaMinas
+{
+    partial class frmBuscaMinas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBuscaMinas));
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Location = new System.Drawing.Point(172, 677);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(118, 48);
+            this.btnJugar.TabIndex = 0;
+            this.btnJugar.Text = "Jugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            this.btnJugar.Click += new System.EventHandler(this.btnJugar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(479, 677);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(125, 48);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rockwell Condensed", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(270, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 51);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Buscaminas";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::JuegoBuscaMinas.Properties.Resources.emojiJuego;
+            this.pictureBox1.Location = new System.Drawing.Point(306, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(172, 214);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 414);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Buscaminas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(641, 750);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dev by Angel Mora";
+            // 
+            // frmBuscaMinas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::JuegoBuscaMinas.Properties.Resources.wallpaperbuscaminas;
+            this.ClientSize = new System.Drawing.Size(784, 779);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnJugar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmBuscaMinas";
+            this.Text = "BuscaMinas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ frmBuscaMinas.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoBuscaMinas
+{
+    public partial class frmBuscaMinas : Form
+    {
+        private static int intentosJugados;
+        private static int TOTFILAS = 9;
+        private static int TOTCOL = 9;
+        private static int TOTBOMBAS = 12;
+        private static string[,] matriz = new string[TOTFILAS, TOTCOL];
+        public frmBuscaMinas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            Button[,] botones = new Button[TOTFILAS, TOTCOL];//Crea los botones con sus dimensiones
+
+
+            this.groupBox1.Controls.Clear(); //Borrar todos los controles
+
+            crearMatriz(botones);
+            crearBombas(botones);
+            ponerGuiasBombas(botones);
+            copiarMatriz(botones, matriz);
+            encerarBotones(botones);
+        }
+
+        /// <summary>
+        /// Encerar la matriz de botones
+        /// </summary>
+        /// <param name="botones"></param>
+        private void encerarBotones(Button[,] botones)
+        {
+            int totalFilas = botones.GetLength(0);
+            int totalColumnas = botones.GetLength(1);
+            for (int i = 0; i < totalFilas; i++)
+            {
+                for (int j = 0; j < totalColumnas; j++)
+                {
+                    botones[i, j].Text = "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copia la matriz de botones (propiedad Text) en la matriz de string
+        /// </summary>
+        /// <param name="botones"></param>
+        /// <param name="matriz"></param>
+        private void copiarMatriz(Button[,] botones, string[,] matriz)
+        {
+            int totalFilas = botones.GetLength(0);
+            int totalColumnas = botones.GetLength(1);
+            for (int i = 0; i < totalFilas; i++)
+            {
+                for (int j = 0; j < totalColumnas; j++)
+                {
+                    matriz[i, j] = botones[i, j].Text;
+                }
+            }
+        }
+
+        private void validaJuego_Click(object sender, EventArgs e)
+        {
+
+            string texto = (sender as Button).Tag.ToString();
+            string[] aux = texto.Split('-');
+
+            int fila = Int32.Parse(aux[0]);
+            int col = Int32.Parse(aux[1]);
+
+            //mostrar el contenido de la matriz en el boton que se hizo clic
+            (sender as Button).Text = matriz[fila, col];
+
+            //desactivar el boton 
+            if (matriz[fila, col] == "")
+                (sender as Button).Enabled = false;
+            //MessageBox.Show($"Hiciste clic: {texto}");
+
+            //Si presiona boton que contiene una bomba automaticamente el Jugador Pierde y el juego termina
+            if ((sender as Button).Text == "💣")
+            {
+                pictureBox1.Image = JuegoBuscaMinas.Properties.Resources.perdisteemoji;
+                MessageBox.Show("Perdiste");
+                groupBox1.Enabled = false;
+            }
+
+            intentosJugados++;//Cada vez que presiona un boton este aumenta su valor
+
+            //Si el jugador no presiona ninguna bomba este gana la partida y el juego termina 
+            if (intentosJugados == 69)
+            {
+                pictureBox1.Image = JuegoBuscaMinas.Properties.Resources.ganasteemoji;
+                MessageBox.Show("Ganaste");
+                groupBox1.Enabled = false;
+            }
+        }
+
+        private void crearMatriz(Button[,] botones)
+        {
+            int x = 10, y = 10;
+            for (int i = 0; i < botones.GetLength(0); i++)
+            {
+                for (int j = 0; j < botones.GetLength(1); j++)
+                {
+                    botones[i, j] = new Button();
+                    botones[i, j].Text = "";
+                    botones[i, j].Font = new Font(botones[i, j].Font, FontStyle.Bold);
+                    botones[i, j].Cursor = Cursors.Hand;
+                    botones[i, j].Location = new System.Drawing.Point(x, y);
+                    botones[i, j].Size = new System.Drawing.Size(32, 27);
+                    botones[i, j].Click += new System.EventHandler(this.validaJuego_Click);
+                    //Almaceno las posiciones de filas y columnas 
+                    botones[i, j].Tag = i.ToString() + "-" + j.ToString();
+
+                    this.groupBox1.Controls.Add(botones[i, j]);
+                    x += 30;
+                }
+                x = 10; y += 27;
+            }
+
+        }
+
+        private void ponerGuiasBombas(Button[,] botones)
+        {
+            int totalFilas = botones.GetLength(0);
+            int totalColumnas = botones.GetLength(1);
+
+            for (int i = 0; i < totalFilas; i++)
+            {
+                for (int j = 0; j < totalColumnas; j++)
+                {
+
+                    if (botones[i, j].Text != "💣")
+                    {
+                        int bombas = 0;
+                        botones[i, j].Text = "";
+
+                        if (totalColumnas > j + 1)
+                            if (botones[i, j + 1].Text == "💣") //buscando bomba a la derecha de la posición actual
+                                bombas++;
+
+                        if (totalColumnas > j - 1 && j - 1 >= 0)
+                            if (botones[i, j - 1].Text == "💣") //buscando bomba a la izquierda de la posición actual
+                                bombas++;
+
+                        if (totalFilas > i + 1)
+                            if (botones[i + 1, j].Text == "💣") //buscando bomba abajo de la posición actual
+                                bombas++;
+
+                        if (totalFilas > i - 1 && i - 1 >= 0)
+                            if (botones[i - 1, j].Text == "💣") //buscando bomba arriba de la posición actual
+                                bombas++;
+
+                        if (totalColumnas > j + 1 && totalFilas > i - 1 && i - 1 >= 0)
+                        {
+                            if (botones[i - 1, j + 1].Text == "💣") //Buscando bomba en diagonal superior derecha de la posición actual
+                                bombas++;
+                        }
+
+                        if (totalColumnas > j - 1 && j - 1 >= 0 && totalFilas > i - 1 && i - 1 >= 0)
+                        {
+                            if (botones[i - 1, j - 1].Text == "💣") //Buscando bomba en diagonal superior izquierda de la posición actual
+                                bombas++;
+                        }
+
+                        if (totalColumnas > j + 1 && totalFilas > i + 1)
+                        {
+                            if (botones[i + 1, j + 1].Text == "💣") //Buscando bomba en diagonal inferior derecha de la posición actual
+                                bombas++;
+                        }
+
+                        if (totalColumnas > j - 1 && j - 1 >= 0 && totalFilas > i + 1)
+                        {
+                            if (botones[i + 1, j - 1].Text == "💣") //Buscando bomba en diagonal inferior izquierda de la posición actual
+                                bombas++;
+                        }
+
+                        if (bombas > 0)
+                        {
+                            botones[i, j].Text = bombas.ToString(); //mostrando la cantidad de bombas
+                            botones[i, j].ForeColor = Color.Green;
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+
+        private void crearBombas(Button[,] botones)
+        {
+            var seed = Environment.TickCount;
+            var random = new Random(seed);
+
+            for (int i = 1; i < TOTBOMBAS; i++)
+            {
+                var x = random.Next(0, TOTFILAS);
+                var y = random.Next(0, TOTCOL);
+                botones[x, y].Text = "💣";
+                botones[x, y].ForeColor = Color.Red;
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+    }
+} //Desarrollado por Angel Mora
+    
+
